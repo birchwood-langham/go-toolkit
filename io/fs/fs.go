5cfd392e7b817ade6dd5a9e218e00f1ed2240c10
Tan Quach <tan.quach@birchwoodlangham.com>
2019-02-10 20:06:10 +0000

    * Added ListFiles function which lists all files in a given path * Added ListSubFolders which lists all subfolders in a given path

--- io/fs/fs.go
+++ io/fs/fs.go
@@ -1,6 +1,9 @@
 package fs
 
-import "os"
+import (
+	"io/ioutil"
+	"os"
+)
 
 // FileExists checks if a file exists at the given path, the function will return an error if the file exists, but is not accessible
 // otherwise it will return a true or false without any errors
@@ -18,4 +21,43 @@ func FileExists(path string) (bool, error) {
 
 	// if we get to here, then it's because the file exists and we have permission to access it
 	return true, nil
+}
+
+// ListFiles gets a list of only the files in a given path
+func ListFiles(path string) ([]string, error) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make([]string, 0, len(files))
+
+	for _, f := range files {
+		if f.IsDir() {
+			continue
+		}
+		results = append(results, f.Name())
+	}
+
+	return results, nil
+}
+
+// ListSubFolders returns a list of subfolders in a given path
+func ListSubFolders(path string) ([]string, error) {
+	files, err := ioutil.ReadDir(path)
+	if err != nil {
+		return nil, err
+	}
+
+	results := make([]string, 0, len(files))
+
+	for _, f := range files {
+		if !f.IsDir() {
+			continue
+		}
+
+		results = append(results, f.Name())
+	}
+
+	return results, nil
 }
